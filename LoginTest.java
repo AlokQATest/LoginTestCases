e433746efa7ee371bbb1fe808f6c4da6a322b2bb
AlokQATest <alokwrk@gmail.com>
2025-06-22 15:14:27 +0530

    Add files via upload

---
+++ LoginTest.java
@@ -0,0 +1,60 @@
+package LoginProject;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		//Wait for 5 second for elements to load
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://practicetestautomation.com/practice-test-login/");
+		driver.manage().window().maximize();
+		
+		//Case 1 - Valid username and password data
+		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("student");
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Password123");
+		
+		driver.findElement(By.xpath("//button[@id='submit']")).click();
+		
+		driver.findElement(By.linkText("Log out")).click();	
+		
+		
+		// Case 2 - Invalid Username and valid Password
+		
+		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("invalid");
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Password123");
+		
+		driver.findElement(By.xpath("//button[@id='submit']")).click();
+		
+		// Printing error message
+		System.out.println(driver.findElement(By.id("error")).getText());
+		
+		// Case 3 - Valid Username and invalid Password
+		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("student");
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("invalid pass");
+		
+		driver.findElement(By.xpath("//button[@id='submit']")).click();
+		
+		// Printing error message
+		System.out.println(driver.findElement(By.id("error")).getText());
+		
+		
+		// Case 2 - No data
+		
+				
+		driver.findElement(By.xpath("//button[@id='submit']")).click();
+				
+		driver.findElement(By.linkText("Log out")).click();
+		
+		driver.close();
+		
+		
+	}
+
+}
